052823c872df7545eaf5956bedd21e1ee6e797cb
donot-sys <nshekhar_be22@thapar.edu>
2024-11-30 12:34:49 +0530

    Course Project1

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+# Loading the dataset
+data = read.table(unz("C:/Users/hp/Downloads/exdata_data_household_power_consumption.zip", "household_power_consumption.txt"), 
+                  header = TRUE, sep = ";", na.strings = "?")
+View(data)
+
+# Subsetting the dataset
+data=data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+# Combining Date and Time into a single Date-Time object
+data$DateTime=as.POSIXct(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+View(data)
+
+# Plot 1: Global Active Power Histogram
+hist(as.numeric(as.character(data$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.copy(png,filename="plot1.png")
+dev.off()
